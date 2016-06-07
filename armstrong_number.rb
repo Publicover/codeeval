05c2cbc300d9a0d5acb6fff35967c9373bd79fcf
Publicover <jameslpublicover@gmail.com>
2016-06-07 17:05:26 -0400

    partial credit on Armstrong number

--- armstrong_number.rb
+++ armstrong_number.rb
@@ -1,32 +1,27 @@
-# "123"
-# "123".split(//)
 
-num = gets.chomp
-num_array = num.to_s.chars
+File.open(ARGV[0]).each_line do |line|
+  num = line.strip
+  num_array = num.to_s.chars
 
-def armstrong(x)
-  new_array = []
-  sum = 0
+  def armstrong(x)
+    new_array = []
+    sum = 0
 
-  x.each do |i|
-    new_array << i.to_i**x.length
-    puts "new_array = #{new_array}"
+    x.each do |i|
+      new_array << i.to_i**x.length
+    end
+
+    new_array.each do |i|
+      sum += i
+    end
+    sum
   end
 
-  new_array.each do |i|
-    sum += i
-    puts "sum = #{sum}"
+  armstrong_sum = armstrong(num_array)
+
+  if armstrong_sum == num
+    print "True\n"
+  else
+    print "False\n"
   end
-
-  sum
-
-end
-
-armstrong_sum = armstrong(num_array)
-puts "armstrong_sum = #{armstrong_sum}"
-
-if armstrong_sum == num.to_i
-  puts "true"
-else
-  puts "false"
 end
