8b4bf3e7996f529b0413d3509eb362a17a72f0b8
Publicover <jameslpublicover@gmail.com>
2016-06-12 21:51:00 -0400

    armstrong_number accepted

--- Mth_to_last_element.rb
+++ Mth_to_last_element.rb
@@ -6,7 +6,6 @@ file_array = []
 
 File.open("/Users/jpublicover/Desktop/codeeval/Mth_to_last_element.txt").each_line do |line|
   num_array = line.split
-  # puts num_array
   last_num = get_mth_element(num_array)
   puts last_num
 end
--- armstrong_number.rb
+++ armstrong_number.rb
@@ -1,25 +1,22 @@
 
-File.open(ARGV[0]).each_line do |line|
+File.open("/Users/jpublicover/Desktop/codeeval/armstrong_number_nums.txt").each_line do |line|
   num = line.strip
-  num_array = num.to_s.chars
+  num_array = num.chars
 
   def armstrong(x)
     new_array = []
     sum = 0
+    ary_length = x.length
+    x.map! {|x| x.to_i**ary_length}
+    ary_sum = 0
+    ary_sum = x.inject(0) {|sum, i| sum + i}
 
-    x.each do |i|
-      new_array << i.to_i**x.length
-    end
-
-    new_array.each do |i|
-      sum += i
-    end
-    sum
+    ary_sum
   end
 
   armstrong_sum = armstrong(num_array)
 
-  if armstrong_sum == num
+  if armstrong_sum == num.to_i
     print "True\n"
   else
     print "False\n"
