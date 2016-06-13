9e7831d28058390d9f3dc7bb0206ea665e0a7048
Publicover <jameslpublicover@gmail.com>
2016-06-12 23:44:12 -0400

    reverse_and_add accepted.

--- reverse_and_add.rb
+++ reverse_and_add.rb
@@ -1,77 +1,21 @@
-# def reverse_and_add(number, counter)
-#   until number.to_s == number.to_s.reverse
-#     num_string = number.to_s
-#     rev_num_string = number.to_s.reverse
-#     maybe_pal = num_string.to_i + rev_num_string.to_i
-#     number = maybe_pal
-#     puts "#{counter} and #{number}"
-#     counter += 1
-#   end
-#   puts "You did it. It's #{number}"
-#   puts "#{counter - 1}"
-# end
 
+File.open("/Users/jpublicover/Desktop/codeeval/reverse_words_words.txt").each_line do |line|
+  num_string = line.strip
+  num = num_string.to_i
+  num_rev = (num_string.reverse).to_i
+  total = num + num_rev
+  total_maybe_pal = total.to_s
+  counter = 1
 
-# File.open(ARGV[0]).each_line do |line|
-
-# num_string = "2367"
-# num_int = 2367
-# num_int_rev = (num_string.reverse).to_i
-#
-# sum = num_int + num_int_rev
-
-
-def is_palindrome?(sum_string)
-  if sum_string == sum_string.reverse
-    return true
-  else
-    return false
+  until total_maybe_pal == total_maybe_pal.reverse
+      num = total
+      num_rev = ((total.to_s).reverse).to_i
+      total = num + num_rev
+      total_maybe_pal = total.to_s
+      counter += 1
   end
+  puts "#{counter} #{total_maybe_pal}"
 end
-
-def rev_and_add(num)
-  num_int = num.to_i
-  num_int_rev = (num.reverse).to_i
-  sum = num_int + num_int_rev
-  num = sum.to_s
-end
-
-# is_palindrome_check = false
-
-File.open("/Users/jpublicover/Desktop/codeeval/reverse_and_add_nums.txt").each_line do |line|
-  num = line.strip
-  counter = 0
-  test_pal = false
-
-  until test_pal == true
-    x = rev_and_add(num)
-    counter += 1
-    test_pal = is_palindrome?(x)
-  end
-  puts "#{counter} #{num}"
-end
-
-
-def rev_and_add(num)
-  num_int = num.to_i
-  num_int_rev = (num.reverse).to_i
-  sum = num_int + num_int_rev
-  num = sum.to_s
-end
-
-File.open("/Users/jpublicover/Desktop/codeeval/reverse_and_add_nums.txt").each_line do |line|
-  num = line.strip
-  counter = 0
-  until num == num.reverse
-    rev_and_add(num)
-    counter += 1
-  end
-  puts "#{counter} #{num}"
-end
-
-
-
-
 
 
 
